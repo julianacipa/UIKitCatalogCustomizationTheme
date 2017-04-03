ed9166f793f50d24000620c965bbdd84cc2a3990
Juliana Cipa <juli.cipa@gmail.com>
2017-04-03 14:40:23 +0100

    Add Bridging header and prefix files

---
+++ Swift/UIKitCatalog/Bridging-Header.h
@@ -0,0 +1,14 @@
+//
+//  Bridging-Header.h
+//  UIKitCatalog
+//
+//  Created by Juliana Cipa on 03/04/2017.
+//  Copyright © 2017 Apple. All rights reserved.
+//
+
+#ifndef Bridging_Header_h
+#define Bridging_Header_h
+
+@import UIKit;
+
+#endif /* Bridging_Header_h */
